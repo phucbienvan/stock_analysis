0487b0174753dbe292f3b0c0ae1f835f1c6fe4fc
phucbienvan <meotran0805@gmail.com>
2024-03-07 00:32:39 +0700

    create stock

--- app/services/stock_service.py
+++ app/services/stock_service.py
@@ -1,4 +1,6 @@
 from app.schemas.sche_stock import StockCreateRequest
+from pandas_datareader import data
+import datetime, json
 
 class StockService(object):
     __instance = None
@@ -8,5 +10,19 @@ class StockService(object):
 
     @staticmethod
     def create(request_data: StockCreateRequest):
-        print(1345)
-        return 1234
+        start_date = request_data.start_date
+        # start_date = datetime.datetime.fromisoformat(start_date)
+        # start_date = f"{start_date.year},{start_date.month},{start_date.day}"
+
+        end_date = request_data.end_date
+
+        start_date = datetime.datetime(2024,1,1)
+        end_date = datetime.datetime(2024,3,1)
+        # end_date = datetime.datetime.fromisoformat(end_date)
+        # end_date = f"{end_date.year},{end_date.month},{end_date.day}"
+
+        df = data.DataReader(name=request_data.sign, data_source="stooq", start=start_date, end=end_date)
+        print(json(df))
+
+        return json(df)
+    
