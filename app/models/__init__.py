26e7e9a840782a69269bef1417ffc827cc337879
phucbienvan <meotran0805@gmail.com>
2024-03-06 23:53:07 +0700

    update model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,2 @@
 from app.models.model_base import Base  # noqa
-from app.models.model_stock import Stock  # noqa
+from app.models.model_stock import Stocks  # noqa
--- app/models/model_stock.py
+++ app/models/model_stock.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, String, DateTime
 
 from app.models.model_base import BareBaseModel
 
-class Stock(BareBaseModel):
+class Stocks(BareBaseModel):
     sign = Column(String(255), index=True)
     start_date = Column(DateTime)
     end_date = Column(DateTime)
