4d61afa2899bf5740c9c8241ad9aad39d79dc405
phucbienvan <meotran0805@gmail.com>
2024-03-21 23:50:49 +0700

    done test login

--- tests/api/test_login.py
+++ tests/api/test_login.py
@@ -12,8 +12,20 @@ async def test_success(client: TestClient) -> None:
     )
     resp_json = resp.json()
 
-    print("resp_json", resp_json)
-
     assert resp_json['code'] == "000"
     assert resp_json["message"] == "success"
     assert resp_json["data"]["access_token"] is not None
+
+@pytest.mark.asyncio
+async def test_wrong_email(client: TestClient) -> None:
+    resp = await client.post(
+        "/login",
+        json={
+            "email": "phucbv1@example.com",
+            "password": "1234567",
+        },
+    )
+    resp_json = resp.json()
+    print("resp_json",resp_json)
+
+    assert resp_json['detail'] == "Incorrect email or password"
